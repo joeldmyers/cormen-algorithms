39dd9702bf118276c4a0c3f95a6f64a5b41b725c
Joel Myers <joeldmyers@gmail.com>
2020-01-27 22:13:26 -0500

    simple linear search method added - 2.1-3

--- chapters/02/insertion-sort.js
+++ chapters/02/insertion-sort.js
@@ -1,5 +1,4 @@
 const insertionSort = array => {
-  console.log("fired method");
   for (var j = 1; j < array.length; j++) {
     let item = array[j];
     let i = j - 1;
@@ -10,8 +9,35 @@ const insertionSort = array => {
     }
     array[i + 1] = item;
   }
-  console.log("array!", array);
   return array;
 };
 
-module.exports = insertionSort;
+const reverseInsertionSort = array => {
+  for (var j = 1; j < array.length; j++) {
+    let item = array[j];
+    let i = j - 1;
+
+    while (i >= 0 && array[i] < item) {
+      array[i + 1] = array[i];
+      i--;
+    }
+    array[i + 1] = item;
+  }
+  return array;
+};
+
+const linearSearch = (array, num) => {
+  console.log("num", num, "array", array);
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+module.exports = {
+  insertionSort,
+  reverseInsertionSort,
+  linearSearch
+};
--- chapters/02/insertion-sort.test.js
+++ chapters/02/insertion-sort.test.js
@@ -1,8 +1,33 @@
-const insertionSort = require("./insertion-sort");
+const {
+  insertionSort,
+  reverseInsertionSort,
+  linearSearch
+} = require("./insertion-sort");
 
 describe("Insertion Sort Method", () => {
   test("It should properly sort number arrays", () => {
     expect(insertionSort([5, 1, 3])).toEqual([1, 3, 5]);
     expect(insertionSort([5, 2, 4, 6, 1, 3])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
+
+describe("Reverse Insertion Sort Method", () => {
+  test("It should properly sort number arrays but in decreasing order", () => {
+    expect(reverseInsertionSort([5, 1, 3])).toEqual([5, 3, 1]);
+    expect(reverseInsertionSort([5, 2, 4, 6, 1, 3])).toEqual([
+      6,
+      5,
+      4,
+      3,
+      2,
+      1
+    ]);
+  });
+});
+
+describe("Linear Search Method", () => {
+  test("It should properly check if ", () => {
+    expect(linearSearch([5, 1, 3], 3)).toEqual(2);
+    expect(linearSearch([5, 2, 4, 6, 1, 3], 6)).toEqual(3);
   });
 });
